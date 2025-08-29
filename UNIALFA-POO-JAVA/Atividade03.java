9ede8183046f154a1a652d0bbb7891ceefe9e873
Joao Victor Gonçalves Gandin <joaovictorgoncalvesgandin@gmail.com>
2025-08-29 18:00:05 -0300

    Create Atividade03.java

---
+++ UNIALFA-POO-JAVA/Atividade03.java
@@ -0,0 +1,8 @@
+public class Atividade03 {
+    public static void main(String[] args) {
+
+        String versao = System.getProperty("java.version");
+        System.out.println("A Versão do Java instalada é: " + versao);
+        
+    }
+}
