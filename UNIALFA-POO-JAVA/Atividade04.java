900a32a03ab59999689065bc97796ae68ed1de02
Joao Victor Gonçalves Gandin <joaovictorgoncalvesgandin@gmail.com>
2025-08-29 18:15:27 -0300

    Create Atividade04.java

---
+++ UNIALFA-POO-JAVA/Atividade04.java
@@ -0,0 +1,37 @@
+
+
+public class Atividade04 {
+
+    public static void main(String[] args) {
+
+        int i = 5;
+        //pós incremento
+        System.out.println("i++: ");
+        System.out.println("O valor inicial de i: " + i);
+        System.out.println("i++: " + i++);
+        System.out.println("O valor de i após i++: " + i);
+        System.out.println();
+        //pré incremento
+        System.out.println("++i: ");
+        System.out.println("O valor inicial de i: " + i);
+        System.out.println("++i: " + ++i);
+        System.out.println("O valor de i após ++i: " + i);
+        System.out.println();
+        //pós decremento
+        System.out.println("i--: ");
+        System.out.println("O valor inicial de i: " + i);
+        System.out.println("i--: " + i--);
+        System.out.println("O valor de i após i--: " + i);
+        System.out.println();
+        //pré decremento
+        System.out.println("--i: ");
+        System.out.println("O valor inicial de i: " + i);
+        System.out.println("--i: " + --i);
+        System.out.println("O valor de i após --i: " + i);
+        System.out.println();
+
+        
+
+    }
+    
+}
