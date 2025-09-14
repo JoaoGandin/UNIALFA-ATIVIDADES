1a55fe7b077c0474bad99e289626426a98322385
Joao Victor Gonçalves Gandin <joaovictorgoncalvesgandin@gmail.com>
2025-09-14 13:03:08 -0300

    Update Tubarao.java

--- UNIALFA-POO-JAVA/heranca-03_09_25/package/animais/Tubarao.java
+++ UNIALFA-POO-JAVA/heranca-03_09_25/package/animais/Tubarao.java
@@ -7,6 +7,6 @@ public class Tubarao extends Animal {
      // Sobrescreve o método emitirSom da superclasse
     @Override
     public void emitirSom() {
-        System.out.println("O tubarao faz: ??? ");
+        System.out.println("O tubarao emite ondas sonoras ");
     }
 }
